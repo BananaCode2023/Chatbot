ac957eee0cba766013bcc628094b32af3eacebf1
Peter John Reyes <peter.john.reyes0326@gmail.com>
2023-09-01 14:35:00 +0800

    Chat bot

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+function who(){
+  alert("Hi! I am Dave! and I am created by Mr. Peter John Reyes.")
+}
+
+function how(){
+  alert("I currently have no age as of now since I am an AI generated chat bot.")
+}
+
+function where(){
+  alert("I live inside this Web browser as your AI chat mate.")
+}
+
+function make(){
+  alert("Hello! Shout out sayo Lods! See you in the digital world!")
+}
+
+function thanks(){
+  alert("You are very welcome! Come back as soon as possible!")
+}
